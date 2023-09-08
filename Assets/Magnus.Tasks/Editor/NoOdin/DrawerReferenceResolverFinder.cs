0214eab3d888b462185d9e7adf6c593fa4693091
Gaetan Deglorie <gaetan@rhinox.training>
2023-09-08 14:41:03 +0200

    [MOD] Clean-up part 2

---
+++ Assets/Magnus.Tasks/Editor/NoOdin/DrawerReferenceResolverFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Rhinox.GUIUtils.Editor;
+using Rhinox.VOLT.Data;
+
+namespace Rhinox.VOLT.Editor
+{
+    public static class DrawerReferenceResolverFinder
+    {
+        public static IReferenceResolver FindReferenceResolver(this BasePropertyDrawer drawer)
+        {
+            if (!drawer.FindAttribute<SerializedGuidProcessorAttribute>(out GenericHostInfo hostInfo, out var processorAttribute))
+                return null;
+
+            var propertyHelper = MemberHelper.Create<IReferenceResolver>(hostInfo.GetValue(), processorAttribute.MemberName);
+            var referenceResolver = propertyHelper.GetSmartValue();
+            return referenceResolver;
+        }
+    }
+}
